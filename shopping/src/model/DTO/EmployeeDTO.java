81995f41f10ff1f7a9de0ebebf465990c271b9c7
이상오 <@DESKTOP-MFVLMB5>
2021-06-13 21:58:29 +0900

    20210613

---
+++ shopping/src/model/DTO/EmployeeDTO.java
@@ -0,0 +1,75 @@
+package model.DTO;
+
+public class EmployeeDTO {
+ String	employeeId;
+ String	empuserid;
+ String	empPw;
+ String	empName;
+ String	hireDate;
+ String	jobId;
+ String	phNumber;
+ String officeNumber;
+ String	email;
+ String	empAddress;
+public String getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(String employeeId) {
+	this.employeeId = employeeId;
+}
+public String getEmpuserid() {
+	return empuserid;
+}
+public void setEmpuserid(String empuserid) {
+	this.empuserid = empuserid;
+}
+public String getEmpPw() {
+	return empPw;
+}
+public void setEmpPw(String empPw) {
+	this.empPw = empPw;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public String getHireDate() {
+	return hireDate;
+}
+public void setHireDate(String hireDate) {
+	this.hireDate = hireDate;
+}
+public String getJobId() {
+	return jobId;
+}
+public void setJobId(String jobId) {
+	this.jobId = jobId;
+}
+public String getPhNumber() {
+	return phNumber;
+}
+public void setPhNumber(String phNumber) {
+	this.phNumber = phNumber;
+}
+public String getOfficeNumber() {
+	return officeNumber;
+}
+public void setOfficeNumber(String officeNumber) {
+	this.officeNumber = officeNumber;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getEmpAddress() {
+	return empAddress;
+}
+public void setEmpAddress(String empAddress) {
+	this.empAddress = empAddress;
+}
+	
+}
